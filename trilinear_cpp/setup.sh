e12ba570f3a51effd49cc20947f0af9faa97a5e5
Ruixi Fan <ruixi@treat.xyz>
2024-02-02 17:43:19 -0800

    redo

--- trilinear_cpp/setup.sh
+++ trilinear_cpp/setup.sh
@@ -1,7 +1,6 @@
 if [ -z "$CUDA_HOME" ]; then
   echo "CUDA_HOME=$CUDA_HOME"
-  python3 setup.py install
 else
   echo "CUDA_HOME undefined, please define CUDA_HOME like /usr/local/cuda-<version>"
-  exit 1
 fi
+pip install .
